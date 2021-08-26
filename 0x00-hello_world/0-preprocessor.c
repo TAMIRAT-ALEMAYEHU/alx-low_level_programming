74f4c7e2c3e281caf0651d2d35c315e00ae81f53
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-26 12:44:06 -0700

    edtied

--- 0x00-hello_world/0-preprocessor.c
+++ 0x00-hello_world/0-preprocessor.c
@@ -1,12 +1,3 @@
 #!/bin/bash
-#include<studio.h>
-int main ()
-{
-  printf("Hello This is TAMIRAT-ALEMAYEHU from alx school");
-  returns 0;
- 
-}
-
-gcc -E 0-preprocessor.c
-export CFILE=0-preprocessor.c
+ printf("Hello This is TAMIRAT from alx school" ) | gcc -E $CFILE > c 
      
