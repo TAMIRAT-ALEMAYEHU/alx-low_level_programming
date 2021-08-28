58a7410c731eb2956a0eb387682116e415591914
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-28 13:06:29 -0700

    edited file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
-void main()
+#include <stdio.h>
+/**
+ * main - functions for alphabets
+ *
+ * Return: 0
+ *
+ * Description : displayes all small letters
+ * except e and q
+ *
+ */
+
+int main(void)
 {
 int ch;
-int putchar(int me);
-me = '.\n';
 ch = 'a';
 while (ch <= 'z')
 {
 putchar(ch);
 if (ch == 'e' || ch == 'q')
 {
-putchar(me);
+putchar('\n');
 ch++;
 }
 else
