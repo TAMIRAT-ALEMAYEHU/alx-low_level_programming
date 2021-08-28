b55ba9bc0bc4d731ad0810be9342e6188d257b7f
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-28 10:33:34 -0700

    edited

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
+/**
+ * main - this function displays the alphabet from a to z
+ *
+ * Return: 0
+ *
+ * Description : displays all alphabet small letters
+*/
+
 int main(void)
 {
 int ch;
 int me;
+int putchar(int);
 me = 'This is my alphabet';
 ch = 'a';
 putchar(me);
