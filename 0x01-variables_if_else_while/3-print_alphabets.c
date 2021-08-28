1b808fafdb4d49315b86cb872d89cadc3e1b71d7
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-28 12:19:01 -0700

    edited

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
-void main()
+#include <stdio.h>
+/**
+ * main - this function work for both upper and lower alphabets to display
+ *
+ * discription : first it displays lowwer case letters then
+ * upper case letters will be displayed
+ *
+ * Return: 0
+ */
+int main(void)
 {
 int ch;
 int putchar(int);
-me = '\n';
 ch = 'a';
 while (ch <= 'z')
 {
@@ -12,7 +20,7 @@ putchar(ch);
 ch++;
 if (ch == 'z')
 {
-putchar(me);
+putchar('\n');
 ch = 'A'
 while (ch < 'Z')
 {
