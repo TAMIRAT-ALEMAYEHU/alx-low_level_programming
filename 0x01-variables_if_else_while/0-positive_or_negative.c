437edb8bdf614de8a2ad28f166f6b69e1e530430
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-28 02:22:26 -0700

    edited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*is_positive - check if a number is greater than 0 
-*The number will be checked
-* 
-*Return: 1 if the number is postitve, or 0 
+*is_positive-check-if-a-numbre-is-greater-than-0 
+*@n-number-will-be-checked 
+*Return:-1-if-the-number-is-postitve,or-0 
 */
 int main(void)
 {
@@ -15,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 /* This is my if and else if code*/
 if (n > 0)
 {
-printf("%d is positive\n", n);
+printf("%dis positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero\n", n);
+printf("%dis zero\n", n);
 }
 else
 {
-printf("%d is negative\n", n);
+printf("%dis negative\n", n);
 }
 return (0);
 }
