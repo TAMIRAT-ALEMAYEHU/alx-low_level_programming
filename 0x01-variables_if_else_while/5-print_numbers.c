9b3135a0b26362157fd660675a7f7092a095ea76
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-29 00:13:44 -0700

    edited

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - this function shows base 10 number
+*
+* Return: 0
+*
+* Description : all program displayes base 10 single digit numbers return
+*/
+
+int main(void)
+{
+int base = 10;
+int expo = 0;
+long double val = 1.0;
+while (expo != 0)
+{
+val *= base;
+printf(val);
+expo++;
+if (val > 9)
+return (0);
+}
+return (0);
+}
