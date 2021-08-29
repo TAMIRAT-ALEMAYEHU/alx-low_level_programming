79647a7647b1c39bb1f93b2586fca986a98f0b26
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-29 12:18:19 -0700

    correcting the mistake

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,16 +12,7 @@ int main(void)
 int num;
 for (num = 0; num < 10; num++)
 {
-if(num == 0)
-{
 putchar(num);
-}
-else if(num < 10)
-{
-putchar(',');
-putchar(' ');
-putchar(num);
-}
 }
 return (0);
 }
