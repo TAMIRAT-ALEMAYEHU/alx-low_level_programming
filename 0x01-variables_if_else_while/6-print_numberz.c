3ff0d7fc5f23a5b6401034a03e78e5825c8b1456
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-29 01:54:18 -0700

    new file added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlin.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - function works for single digit numbers base 10
+ *
+ * Return: 0
+ *
+ * Description - displays base 10 single digit numbers only
+ */
+
+int main(void)
+{
+int expo = 0;
+int val = 0;
+while (val < 10)
+{
+putchar(expo);
+expo++;
+val++;
+}
+putchar('\n');
+return (0);
+}
