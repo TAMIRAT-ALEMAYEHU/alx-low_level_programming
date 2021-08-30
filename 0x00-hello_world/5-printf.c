b6977af1f71d947db5a1929570020781ca89f3f3
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-30 12:27:09 -0700

    new file added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - functions display statements
+* Return: 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
