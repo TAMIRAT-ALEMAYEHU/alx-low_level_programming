e09971d50618733caa1c01e55b7532c09c88fe61
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-30 12:22:23 -0700

    new file added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - function prints the sentences
+* Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
