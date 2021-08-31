0628a562565b3086d14160104a597b166efb670b
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-31 14:08:45 -0700

    edited

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_sign - displayes the sign
+ * _abs - displayes the sign
  * @n: the number
  * Return: 1, 0, or -1
  */
 
-int print_sign(int n)
+int _abs(int n)
 {
 if (n > 0)
 {
