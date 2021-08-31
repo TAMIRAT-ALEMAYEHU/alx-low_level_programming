aad7b0145da888de7dce9658a98cefaf44f6ad24
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-31 14:16:05 -0700

    edited

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,26 +1,15 @@
 #include "main.h"
 
 /**
- * _abs - displayes the sign
+ * _abs - displayes the absolute value of int
  * @n: the number
- * Return: 1, 0, or -1
+ * Return: absolute value
  */
 
 int _abs(int n)
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-_putchar('0');
-return (0);
-}
+if (n >= 0)
+return (n);
 else
-{
-_putchar('-');
-return (-1);
-}
+return (-n);
 }
