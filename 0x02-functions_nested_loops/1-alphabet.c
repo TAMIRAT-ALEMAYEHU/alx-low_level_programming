014d6752a091af7c3e852fbd62e8a1ea580d922e
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-31 12:30:24 -0700

    edited

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,15 @@
  * main - function displays alphabet in lowercase
  * Return: 0
  */
+void int main(void)
+{
 void print_alphabet(void)
 {
 char letr;
 for (letr = 'a'; letr <= 'z'; letr++)
- _putchar(letr);
- _putchar('\n');
-return (0);
+{
+_putchar(letr);
+_putchar('\n');
+}
+}
 }
