4b69fc9fbf988f0b4102a946096a375a49e469c8
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-31 10:03:18 -0700

    edited

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,22 @@
-#include <unistd.h>
-#include <main.h>
+#include "holberton.h"
+
 /**
- * _putchar - writes the character c to stdout
+ * main - function to display Holberton 
  * @c: The character to print
  *
- * Return: On sucess 0
+ * Return: 0
  */
-int _putchar(char c)
+int main(void)
 {
-c = "Holberton";
-return (write(1, $c, 1));
+_putchar('H');
+_putchar('0');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
 }
