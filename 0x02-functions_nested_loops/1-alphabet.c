61b3e0012cbcaae6cc7062135bae3477bb472ae6
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-31 12:25:55 -0700

    adding new file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - function displays alphabet in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char letr;
+for (letr = 'a'; letr <= 'z'; letr++)
+ _putchar(letr);
+ _putchar('\n');
+return (0);
+}
