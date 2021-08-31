4f50778830de15879aced55cc96bb5146a60173d
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-31 13:05:06 -0700

    new file added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - function check weather the letter is lowercase of not
+ *
+ * Return: 1 otherwise 0
+ */
+
+int _islower(int c)
+{
+if(c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
