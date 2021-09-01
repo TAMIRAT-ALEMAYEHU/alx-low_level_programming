8f9e71e93049ab06220eb0f82bd820b4165a25f1
Tamirat <88383918+TAMIRAT-ALEMAYEHU@users.noreply.github.com>
2021-09-01 17:32:31 +0300

    edited

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * main - functiolns
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
