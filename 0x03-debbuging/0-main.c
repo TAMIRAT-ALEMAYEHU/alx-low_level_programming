64a7cc8c5498c58db699b98301571f4f4bb8ed00
Tamirat <88383918+TAMIRAT-ALEMAYEHU@users.noreply.github.com>
2021-09-03 21:47:20 +0300

    edited

--- 0x03-debbuging/0-main.c
+++ 0x03-debbuging/0-main.c
@@ -3,13 +3,11 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
 int i;
 i = 98;
 if (i < 0)
 positive_or_negative(i);
-else
 return (0);
 }
