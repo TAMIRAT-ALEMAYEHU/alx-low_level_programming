97be732564d6ee603722c612645cece75cc9d906
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-09-05 13:39:34 -0700

    edited

--- 0x03-debbuging/0-main.c
+++ 0x03-debbuging/0-main.c
@@ -9,5 +9,20 @@ int i;
 i = 98;
 if (i < 0)
 positive_or_negative(i);
-return (1);
+return (0);
+int positive_or_negative(int i)
+{
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
+}
 }
