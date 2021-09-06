452caecd21c23b2b4e998a78bdf48dca5cfe0e93
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-09-06 11:35:32 -0700

    edited

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-* reset_to_98 - Takes a pointer to an int and updates
-*               the value it points to to 98
-* @n: The pointer to an int.
-*/
+ * reset_to_98 - takes a pointer
+ *@n: The pointer
+ */
 void reset_to_98(int *n)
 {
 *n = 98;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdlib.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -14,5 +12,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
 #endif
