176cdf8d46db9b59d315a7ff6e48736151a509a1
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-09-07 11:15:30 -0700

    edited

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,8 +6,8 @@
  */
 void print_rev(char *s)
 {
-int len = 0, index;
-for (index = len - 1; index >= 0; index--)
+int len[] = "Holberton!", index;
+for (index = len; index >= 9; index--)
 {
 len = s[index++];
 s[len - 1 - index] = s[index];
