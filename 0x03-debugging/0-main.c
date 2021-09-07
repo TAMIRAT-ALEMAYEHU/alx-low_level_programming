9094e40f39608a7dd3b9e6f8fa3bb3662ccde68d
Tamirat <88383918+TAMIRAT-ALEMAYEHU@users.noreply.github.com>
2021-09-07 21:52:14 +0300

    edited

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,26 +4,13 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
+
 int main(void)
 {
-int i;
-i = 98;
-if (i < 0)
-positive_or_negative(i);
-return (0);
-int positive_or_negative(int i)
-{
-if (i > 0)
-{
-printf("%d is positive\n", i);
-}
-else if (i == 0)
-{
-printf("%d is zero\n", i);
-}
-else
-{
-printf("%d is negative\n", i);
-}
-}
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
