3e4ec73178e74fa6161f7fe86f917ae7677a71ba
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-09-13 11:46:11 -0700

    edited

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,9 +11,9 @@
 
 char *_memset(char *s, char b, unsigned int  n)
 {
-unsigned int index;
-char *memory = s, value = b;
-for ( index = 0; index < n; index++ )
+unsigned int index, value = b;
+char *memory = s;
+for (index = 0; index < n; index++)
 memory[index] = value;
 return (memory);
 }
