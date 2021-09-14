78257974ce52b5fa2fc31d8adab6d82aab3a5878
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-09-14 12:08:48 -0700

    edited

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	char *destination = dest;
-	char *source = src;
-
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
-
-	return (dest);
+unsigned int index;
+char *destination = dest;
+char *source = src;
+for (index = 0; index < n; index++)
+destination[index] = source[index];
+return (dest);
 }
