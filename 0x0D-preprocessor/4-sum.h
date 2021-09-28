7f8e81a38a8f159552f6644a386edf7adb2465cb
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-09-28 10:20:18 -0700

    edited

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Description: Header file that defines a function-like
+ *                 macro SUM(x, y) that copmutes the sum
+ *                  of the number x and y.
+ *?
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
