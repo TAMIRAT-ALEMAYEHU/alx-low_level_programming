a16d2fb0ece98c4eb551ab9c26c00fd3a0a4d649
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-09-28 09:52:16 -0700

    edited

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file of the
+ *        program was compiled.
+ *
+ * Description: the program that prints
+ *                 the name of the file compiled
+ *
+ * Return : always 0
+ */
+
+int main(void)
+{
+printf("%\n", __FILE__);
+return (0);
+}
