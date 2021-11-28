1d27e884f97bbab6e174f0d08959c53508ead74e
Tamirat <88383918+TAMIRAT-ALEMAYEHU@users.noreply.github.com>
2021-11-29 00:21:04 +0300

    edited

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,22 @@
+#include "lists.h"
+
+**
+ * pint_dlistint - Pints all the elements of a dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Retun: The numbe of nodes in the list.
+ *
+size_t pint_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		pintf("%d\n", h->n);
+		h = h->next;
+	}
+
+	etun (nodes);
+}
 
